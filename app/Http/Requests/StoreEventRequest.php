7717f066f103e2afad7d8deedf705284ee2da3a0
mohamedegila <mohammedegila@gmail.com>
2023-01-24 18:53:36 +0200

    fix validation

--- app/Http/Requests/StoreEventRequest.php
+++ app/Http/Requests/StoreEventRequest.php
@@ -28,7 +28,7 @@ class StoreEventRequest extends FormRequest
             "description" => "required",
             "start_date"  => "required|date|after_or_equal:today",
             "end_date"  => "required|date|after_or_equal:start_date",
-            "start_time"=> "required|date_format:H:i:s",
+            "start_time"=> "required|date_format:H:i",
             "end_time"=> "required|after:start_time",
             "duration"=> "required|numeric",
             "location" => "required",
